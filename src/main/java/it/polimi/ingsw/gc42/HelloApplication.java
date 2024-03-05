82d1a67aaa2969864c029dcb5f99df19a65b7b4b
Cristian Summa <cristian.summa@mail.polimi.it>
2024-03-05 20:36:22 +0100

    Added Window Title

--- src/main/java/it/polimi/ingsw/gc42/HelloApplication.java
+++ src/main/java/it/polimi/ingsw/gc42/HelloApplication.java
@@ -7,15 +7,16 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Codex Naturalis");
         stage.setScene(scene);
-        stage.getIcons().add(new Image(getClass().getResourceAsStream("images/logo.png")));
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/logo.png"))));
         stage.show();
     }
 
