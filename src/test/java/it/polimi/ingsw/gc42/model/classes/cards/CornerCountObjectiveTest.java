2d912e48a238eb2f107ea9cc4f013be33ee6a663
Cristian Summa <cristian.summa@mail.polimi.it>
2024-05-21 23:06:00 +0200

    Fixed an error in Test

--- src/test/java/it/polimi/ingsw/gc42/model/classes/cards/CornerCountObjectiveTest.java
+++ src/test/java/it/polimi/ingsw/gc42/model/classes/cards/CornerCountObjectiveTest.java
@@ -17,28 +17,12 @@ class CornerCountObjectiveTest {
         Game g = new Game();
         Player p = new Player(Token.BLUE);
 
-        try {
-            p.drawCard(g.getResourcePlayingDeck());
-            p.drawCard(g.getResourcePlayingDeck());
-            p.drawCard(g.getResourcePlayingDeck());
-        } catch (IllegalActionException e) {
-            e.printStackTrace();
-        }
-
-        ArrayList<Card> starters = g.getStarterDeck().getCopy();
-        for (Card s: starters) {
-            if (s.getId() == 84) {
-                s.flip();
-                p.setTemporaryStarterCard((StarterCard) s);
-                p.setStarterCard();
-            }
-        }
         GoldCard gold = null;
         Card c1 = null;
         Card c2 = null;
 
-        for(Card c : g.getGoldPlayingDeck().getDeck().getCopy()) {
-            if(c.getId() == 46) {
+        for (Card c : g.getGoldPlayingDeck().getDeck().getCopy()) {
+            if (c.getId() == 46) {
                 gold = (GoldCard) c;
             }
         }
@@ -54,35 +38,51 @@ class CornerCountObjectiveTest {
                 }
             }
         }
-        for (Card c: g.getResourcePlayingDeck().getDeck().getCopy()) {
-            if(c.getId() == 36) {
+        for (Card c : g.getResourcePlayingDeck().getDeck().getCopy()) {
+            if (c.getId() == 36) {
                 c1 = c;
-            } else if(c.getId() == 10) {
+            } else if (c.getId() == 10) {
                 c2 = c;
             }
         }
-        if (null == c1 || null == c2) {
-            Card c = g.getResourcePlayingDeck().getSlot(1);
-            if (c.getId() == 36) {
-                c1 = c;
-            } else if(c.getId() == 10) {
-                c2 = c;
-            }
-        } else {
-            Card c = g.getResourcePlayingDeck().getSlot(2);
-            if (c.getId() == 36) {
-                c1 = c;
-            } else if(c.getId() == 10) {
-                c2 = c;
+
+        Card c = g.getResourcePlayingDeck().getSlot(1);
+        if (c.getId() == 36) {
+            c1 = c;
+        } else if (c.getId() == 10) {
+            c2 = c;
+        }
+        c = g.getResourcePlayingDeck().getSlot(2);
+        if (c.getId() == 36) {
+            c1 = c;
+        } else if (c.getId() == 10) {
+            c2 = c;
+        }
+
+        try {
+            p.drawCard(g.getResourcePlayingDeck());
+            p.drawCard(g.getResourcePlayingDeck());
+            p.drawCard(g.getResourcePlayingDeck());
+        } catch (IllegalActionException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<Card> starters = g.getStarterDeck().getCopy();
+        for (Card s : starters) {
+            if (s.getId() == 84) {
+                s.flip();
+                p.setTemporaryStarterCard((StarterCard) s);
+                p.setStarterCard();
             }
         }
+
         try {
             p.setHandCard(0, (PlayableCard) c1);
             p.setHandCard(1, (PlayableCard) c2);
             p.setHandCard(2, gold);
             p.playCard(1, 1, 0);
             p.drawCard(g.getResourcePlayingDeck());
-            p.playCard(1,  0, 1);
+            p.playCard(1, 0, 1);
             p.drawCard(g.getResourcePlayingDeck());
             p.playCard(1, 1, 1);
             p.drawCard(g.getResourcePlayingDeck());
